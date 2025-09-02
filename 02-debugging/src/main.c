3c854b22e2ba932283343269a416baf6ab66bd9d
JoeyBlount <joeyblount@csus.edu>
2025-09-02 16:13:56 -0700

    phase0Complete

--- 02-debugging/src/main.c
+++ 02-debugging/src/main.c
@@ -6,8 +6,11 @@
  * @return value indicating the length of the string
  */
 int strlen(char *str) {
-    //Implement me!
-    return 0;
+    int len = 0;
+    while(str[len] != '\0') {
+	len++;
+    }
+    return len;
 }
 
 /**
